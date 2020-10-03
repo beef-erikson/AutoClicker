1c9f173d18660000742dd8be95a87e05285a7042
Troy Martin <beef.erikson.studios@gmail.com>
2020-10-03 04:34:53 -0400

    Add project files.

---
+++ AutoClicker/Hotkeys.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+//using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace AutoClicker
+{
+    public class GlobalHotkey : Window
+    {
+        // Fields 
+        private int modifier;
+	    private int key;
+	    private IntPtr handler;
+	    private int id;
+        
+        // Register hotkey
+        [DllImport("user32.dll")]
+        static extern bool RegisterHotKey(IntPtr windowsHandler, int hotkey_id, int Modifiers, int virtualkey);
+
+        // Unregister hotkey
+        [DllImport("user32.dll")]
+        static extern bool UnregisterHotKey(IntPtr windowsHandler, int hotkey_id);
+
+
+    /*
+	    public GlobalHotkey(int modifier, Keys key, Form form)
+	    {
+	        this.modifier = modifier;
+	        this.key = (int) key;
+            this.handler = form.Handle;
+	        id = this.GetHashCode();
+	    }
+    */
+
+    /// <summary>
+    /// Constants for storing keys
+    /// See https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+    /// </summary>
+    public static class Constants
+        {
+            // Modifiers
+            public const int NOMOD = 0x0000;
+            public const int ALT = 0x0001;
+            public const int CTRL = 0x0002;
+            public const int SHIFT = 0x0004;
+            public const int WIN = 0x0008;
+
+            // Keys
+            public const int KEY_F1 = 0x70;
+            public const int KEY_F2 = 0x71;
+            public const int KEY_F3 = 0x72;
+            public const int KEY_F4 = 0x73;
+            public const int KEY_F5 = 0x74;
+            public const int KEY_F6 = 0x75;
+            public const int KEY_F7 = 0x76;
+            public const int KEY_F8 = 0x77;
+            public const int KEY_F9 = 0x78;
+            public const int KEY_F10 = 0x79;
+            public const int KEY_F11 = 0x7A;
+            public const int KEY_F12 = 0x7B;
+
+            // Windows message id for hotkey
+            public const int WM_HOTKEY_MSG_ID = 0x0312;
+        }
+    }
+}
---
+++ AutoClicker/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Timer min and max values
+        const float MINTIME = 1;
+        const float MAXTIME = 360000;
+
+        // Timer
+        static System.Timers.Timer theTimer;
+
+        // Mouse click fields
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        // Mouse coordinate fields
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        
+        // Mouse points
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+
+        /// <summary>
+        /// Main method
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Gets current mouse position in X, Y
+        /// </summary>
+        /// <returns>Mouse point</returns>
+        public static Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+
+        /// <summary>
+        /// Forces mouse click
+        /// </summary>
+        private static void DoMouseClick()
+        {
+            // Call the imported function with the cursor's current position
+            Point mousePoint = GetMousePosition();
+            uint X = (uint)mousePoint.X;
+            uint Y = (uint)mousePoint.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+
+        /// <summary>
+        /// Handles timer checking and button labels.
+        /// Passes to StartTimer.
+        /// </summary>
+        private void ProcessTimer()
+        {
+            // "Start" has been clicked
+            if ((string)TimerButton.Content == "Start")
+            {
+                // Change button label
+                TimerButton.Content = "Stop";
+
+                // Checks to make sure value is float and within parameters before passing to StartTimer
+                float f;
+
+                if (float.TryParse(TimerTextBox.Text, out f) && f >= MINTIME && f <= MAXTIME)
+                    StartTimer(f);
+                else
+                    MessageBox.Show($"Value must be a valid number between {MINTIME:n0} and {MAXTIME:n0}.", "Error");
+            }
+            // "Stop" has been clicked
+            else
+            {
+                TimerButton.Content = "Start";
+                StopTimer();
+            }
+        }
+
+
+        /// <summary>
+        /// Starts the timer / mouse clicking.
+        /// </summary>
+        /// <param name="setTime">Amount of time in between clicks</param>
+        private void StartTimer(float setTime)
+        {
+            // Sets and starts timer
+            theTimer = new System.Timers.Timer(setTime);
+            theTimer.Elapsed += ClickMouse;
+            theTimer.Enabled = true;
+        }
+
+
+        /// <summary>
+        /// Stops timer / mouse clicking.
+        /// </summary>
+        private void StopTimer()
+        {
+            theTimer.Stop();
+            theTimer.Dispose();
+        }
+
+
+        /// <summary>
+        /// Processes the mouse clicks.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e">Elapsed time</param>
+        private static void ClickMouse(Object source, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("{0:HH:mm:ss.fff}", e.SignalTime);
+            DoMouseClick();
+        }
+
+
+        /// <summary>
+        /// Button has been clicked; start timer and change text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessTimer();
+        }
+    }
+}
